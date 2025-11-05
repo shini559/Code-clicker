a6581fd063f548bebf0b3245c949820b6d88e428
wassim <wassim.motasali@epitech.eu>
2025-11-05 10:47:01 +0100

    first commit

---
+++ idle_clicker/android/app/src/main/kotlin/com/example/idle_clicker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.idle_clicker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ idle_clicker/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CodeClicker extends StatefulWidget {
+  const CodeClicker({super.key});
+
+  @override
+  State<CodeClicker> createState() => _CodeClickerState();
+}
+
+class _CodeClickerState extends State<CodeClicker> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
